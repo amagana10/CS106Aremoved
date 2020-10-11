567ddc7956498ea8b3f232291f7f0f88f0801630
higebre16 <higebre16@gmail.com>
2020-10-11 15:16:28 -0500

    updated DeletingCharacterFromString

--- Assignment4/src/DeletingCharacterFromString.java
+++ Assignment4/src/DeletingCharacterFromString.java
@@ -1,9 +1,9 @@
 
 public class DeletingCharacterFromString {
 	
-	public String removelAllOccurrences(String str, char ch) {
+	public static String removelAllOccurrences(String str, char ch) {
 		String result = "";
-		for (int i = 0; i< str.length(), i++) {
+		for (int i = 0; i< str.length(); i++) {
 			if (str.charAt(i)!= ch) {
 				result += str.charAt(i);
 			}
@@ -12,7 +12,7 @@ public class DeletingCharacterFromString {
 		
 		return result;
 	}
- public void run() {
+ public static void main(String[] args) {
 	 
 	System.out.println(removelAllOccurrences("This is a test", 't'));
 	System.out.println(removelAllOccurrences("summer is a here!", 'e'));
