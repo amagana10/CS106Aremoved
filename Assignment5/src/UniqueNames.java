ad11c55aff70b52059a56b27682bd99743919691
higebre16 <higebre16@gmail.com>
2020-10-12 13:20:25 -0500

    Assignment5 for week4

---
+++ Assignment5/src/UniqueNames.java
@@ -0,0 +1,27 @@
+import acm.program.*;
+import java. util.*;
+
+public class UniqueNames extends ConsoleProgram{
+	
+	public void run() {
+		ArrayList<String> list = new ArrayList<String>();	
+		while(true) {
+			String name = readLine(" Enter name:");
+			if (name.equals("")) break;
+			if (!list.contains(name)) {
+				list.add(name);
+				
+			}
+			
+		}
+		printList("Unique name list conttains:");
+		printList(list);
+		
+	}
+private void printList(ArrayList list) {
+	for(int i =0; i<list.size(); i++) {
+		printList(list.get(i));
+		
+	}
+}
+}
