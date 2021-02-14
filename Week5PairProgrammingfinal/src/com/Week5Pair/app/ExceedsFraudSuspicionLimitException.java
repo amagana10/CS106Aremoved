e48864e29bd8f0ef4242716ed139af2b569fdb7e
amagana10 <32153064+amagana10@users.noreply.github.com>
2021-02-14 12:55:12 -0600

    Delete Week5PairProgrammingfinal directory

--- Week5PairProgrammingfinal/src/com/Week5Pair/app/ExceedsFraudSuspicionLimitException.java
+++
@@ -1,9 +0,0 @@
-package com.Week5Pair.app;
-
-public class ExceedsFraudSuspicionLimitException extends Exception {
-	
-	ExceedsFraudSuspicionLimitException(String errorMessage){
-		super(errorMessage);
-	}
-	
-}
